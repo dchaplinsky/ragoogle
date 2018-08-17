e353b58238fe6bac42e325bff544c4cd69c74fc3
Dmitry Chaplinsky <dchaplinsky@conversionscience.co.uk>
2018-08-18 02:49:09 +0300

    Microformats for vkks

--- jinja2_env.py
+++ jinja2_env.py
@@ -7,8 +7,9 @@ from django.utils.translation import gettext, ngettext
 
 from dateutil.parser import parse as parse_dt
 from jinja2 import Environment, evalcontextfilter, Markup, escape
+from names_translator.name_utils import parse_and_generate, generate_all_names
 
-_paragraph_re = re.compile(r'(?:\r\n|\r|\n){2,}')
+_paragraph_re = re.compile(r"(?:\r\n|\r|\n){2,}")
 
 
 def updated_querystring(request, params):
@@ -24,29 +25,54 @@ def updated_querystring(request, params):
 
 @evalcontextfilter
 def nl2br(eval_ctx, value):
-    result = u'\n\n'.join(u'<p>%s</p>' % p.strip().replace('\n', Markup('<br>\n'))
-                          for p in _paragraph_re.split(escape(value)))
+    result = u"\n\n".join(
+        u"<p>%s</p>" % p.strip().replace("\n", Markup("<br>\n"))
+        for p in _paragraph_re.split(escape(value))
+    )
     if eval_ctx.autoescape:
         result = Markup(result)
     return result
 
+
+def identify_relation(rel):
+    rel = rel.lower().strip()
+    if rel in ["дружина", "чоловік"]:
+        return "spouse"
+
+    if rel in ["брат", "сестра", "двоюрідний брат", "двоюрідна сестра", "рідний брат", "рідна сестра"]:
+        return "sibling"
+
+    if rel in ["мати", "батько"]:
+        return "parent"
+
+    if rel in ["син", "дочка", "донька", "пасинок"]:
+        return "children"
+
+    return "knows"
+
 def environment(**options):
     env = Environment(**options)
-    env.globals.update({
-        'static': staticfiles_storage.url,
-        'url': reverse,
-    })
-    env.install_gettext_callables(
-        gettext=gettext, ngettext=ngettext, newstyle=True
-    )
+    env.globals.update({"static": staticfiles_storage.url, "url": reverse})
+    env.install_gettext_callables(gettext=gettext, ngettext=ngettext, newstyle=True)
 
-    env.filters.update({
-        'datetime': lambda dt: formats.date_format(parse_dt(dt) if isinstance(dt, str) else dt, "SHORT_DATETIME_FORMAT"),
-        'date': lambda dt: formats.date_format(parse_dt(dt) if isinstance(dt, str) else dt, "SHORT_DATE_FORMAT"),
-        'nl2br': nl2br
-    })
-    env.globals.update({
-        'updated_querystring': updated_querystring
-    })
+    env.filters.update(
+        {
+            "datetime": lambda dt: formats.date_format(
+                parse_dt(dt) if isinstance(dt, str) else dt, "SHORT_DATETIME_FORMAT"
+            ),
+            "date": lambda dt: formats.date_format(
+                parse_dt(dt) if isinstance(dt, str) else dt, "SHORT_DATE_FORMAT"
+            ),
+            "nl2br": nl2br,
+            "identify_relation": identify_relation
+        }
+    )
+    env.globals.update(
+        {
+            "updated_querystring": updated_querystring,
+            "parse_and_generate": parse_and_generate,
+            "generate_all_names": generate_all_names,
+        }
+    )
 
     return env
